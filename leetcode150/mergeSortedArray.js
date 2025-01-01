87c6703394a7c5846c5c01fece58431613e677bf
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-01-01 16:13:16 +0530

    add merge sorted array solution

--- leetcode150/mergeSortedArray.js
+++ leetcode150/mergeSortedArray.js
@@ -2,29 +2,27 @@
  * link; https://leetcode.com/problems/merge-sorted-array/
  Do not return anything, modify nums1 in-place instead.
  */
-function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let i = 0;
-    let j =0;
-    let right = m;
-    while(i< nums1.length && j < n ){
-        if(nums1[i]==0){
-            nums1[i] = nums2[j]
-            i++;
-            j++
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let i = m-1;
+    let j = n-1;
+    let k = m+n-1;
+
+    while(j>=0){
+        if(i>=0 && nums1[i]> nums2[j]){
+            nums1[k]= nums1[i];
+            i--
         }
-        else if(nums1[i]<=nums2[j]){
-            i++;
+        else{
+            nums1[k] = nums2[j];
+            j--;
         }
-        else {
-            nums1[right] = nums1[i];
-            nums1[i]= nums2[j];
-            j++;
-            i++;
-            right++
-        }
-    }
-    // for(let c1 = j;c1< n;c1++){
-    //     nums1[right] = nums2[c1];
-    //     right++;
-    // }                       
+        k--;
+    }  
 };
