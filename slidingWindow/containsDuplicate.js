f79bfb00a09a3b5c16a00a7a90b785ca15e5b12d
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-01-13 21:18:33 +0530

    add alteranate solution for contains duplicate II

--- slidingWindow/containsDuplicate.js
+++ slidingWindow/containsDuplicate.js
@@ -8,8 +8,7 @@ var containsNearbyDuplicate = function(nums, k) {
     let map = {};
     for(let i=0;i< Math.min(nums.length, k+1);i++){
         if(map[nums[i]]!=undefined){
-            return true;
-        }
+            return true; }
         map[nums[i]] =i;
     }
     for(let i = k+1;i<nums.length;i++){
@@ -18,6 +17,20 @@ var containsNearbyDuplicate = function(nums, k) {
             return true;
         }
         map[nums[i]] = i;
+    }
+    return false;
+};
+
+
+// small optimization
+var containsNearbyDuplicate = function(nums, k) {
+    let map = {};
+    let left = 0;
+    for(let right = 0;right< nums.length;right++){
+        if(map[nums[right]]!= undefined && (right-map[nums[right]])<=k){
+            return true;
+        }
+        map[nums[right]]=right;
     }
     return false;
 };
