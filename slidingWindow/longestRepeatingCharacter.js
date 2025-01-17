01b6f7b31527acfb7029cf019bba5a7776a5abde
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-01-17 12:02:59 +0530

    add max repeating character replacement solution

--- slidingWindow/longestRepeatingCharacter.js
+++ slidingWindow/longestRepeatingCharacter.js
@@ -4,19 +4,22 @@
  * @param {number} k
  * @return {number}
  */
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
 var characterReplacement = function(s, k) {
      let left = 0;
     let map = {};
     let maxLength = 0;
     let count = 0;
     for(let right = 0;right < s.length;right++){
-      let rightChar = s.charAt(right);
-      map[rightChar] = (map[rightChar] || 0) + 1;
-      count = Math.max(count, map[rightChar]);
+      map[s[right]] = (map[s[right]] || 0) + 1;
+      count = Math.max(count, map[s[right]]);
 
       if( right-left+ 1 - count  > k ){
-        let leftChar = s.charAt(left);
-        map[leftChar] = map[leftChar]-1;
+        map[s[left]] = map[s[left]]-1;
         left ++;
         count --;
       }
