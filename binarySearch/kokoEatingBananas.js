c2bec47af1d0481f732573ebfd7d371da336a918
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-04-08 09:01:08 +0530

    add solution for koko eating bananas

---
+++ binarySearch/kokoEatingBananas.js
@@ -0,0 +1,29 @@
+/**
+ * link: https://leetcode.com/problems/koko-eating-bananas/
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    let left = 1;
+    let right = Math.max(...piles)
+    while(left <= right){
+        let mid = Math.floor((left+right)/2);
+        let midVal = piles[mid];
+        if(canEatFeasibly(piles, h, mid)){
+            right = mid-1
+        }
+        else{
+            left = mid+1
+        }
+    }
+    return left;
+};
+
+const canEatFeasibly = function(piles,h, val) {
+    let hours = 0;
+    for(const banana of piles){
+        hours = hours+ Math.ceil(banana/val)
+    }
+    return hours <=h;
+}
