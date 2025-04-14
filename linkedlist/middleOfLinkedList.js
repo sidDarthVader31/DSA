8370d1d1896cf05eb05d85543e2536f4d718dcba
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-04-14 21:40:04 +0530

    add solution for middle of a linked list: beats 100%

---
+++ linkedlist/middleOfLinkedList.js
@@ -0,0 +1,28 @@
+/**
+ * link: https://leetcode.com/problems/middle-of-the-linked-list/description/
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let fast = head;
+    let slow = head;
+    while(fast?.next){
+        slow = slow.next;
+        if(fast?.next?.next){
+        fast = fast?.next?.next;
+        }
+        else{
+            break;
+        }
+       
+    }
+    return slow
+};
+
