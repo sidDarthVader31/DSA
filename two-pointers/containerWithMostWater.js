af2162cb013eb1aea3a8581a433355159553257a
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-04-29 10:44:36 +0530

    add container with most water

--- two-pointers/containerWithMostWater.js
+++ two-pointers/containerWithMostWater.js
@@ -3,19 +3,21 @@
  * @param {number[]} height
  * @return {number}
  */
+
 var maxArea = function(height) {
-   let maxArea = 0;
-   let left =0;
-   let right = height.length-1;
-    while(left < right){
-    let area = Math.min(height[left], height[right]) * Math.abs(right-left);
-    maxArea = Math.max(area, maxArea);
-    if(height[left]< height[right]){
-        left++
+    let left = 0;
+    let right = height.length-1;
+    let maxArea = 0;
+    while(left<=right){
+        let area = Math.min(height[left], height[right]) * (right-left);
+        maxArea = Math.max(maxArea, area);
+
+        if(height[left]> height[right]){
+            right--
+        }
+        else{
+            left++;
+        }
     }
-    else{
-        right--
-    }
-   }
-   return maxArea;
-};
+    return maxArea;
+}
