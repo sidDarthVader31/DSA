ce597ed103ab230740c42c05ca6d9d26ca815583
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-05-03 12:34:35 +0530

    add merge two sorted list solution via recursion

--- linkedlist/mergeTwoSortedList.js
+++ linkedlist/mergeTwoSortedList.js
@@ -50,3 +50,28 @@ var mergeTwoLists = function(list1, list2) {
      
     return prev;
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoListsRecursion = function(list1, list2, head) {
+   //base case 
+   if(!list1 || !list2) {
+    return list1 ? list1 : list2;
+   }
+   if(list1.val > list2.val ){
+    [list1, list2] = [list2, list1]; //swap
+   }
+   list1.next = mergeTwoLists(list1.next, list2);
+   return list1
+};
