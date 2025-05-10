0cb85c90c7fa7e7f25bb8a5646525b998d4cd448
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-05-10 19:39:38 +0530

    add bfs algorithm level wise output

--- trees/traversal.js
+++ trees/traversal.js
@@ -22,3 +22,40 @@ const breadthFirstSearch = (root) =>{
   }
   return list;
 }
+
+
+//a more sophisticated approach - 
+//this allows us to see all the nodes at any particular
+//level
+//
+
+const bfs = (root) => {
+  let currentNode = root;
+  let list = [];
+  let queue = [];
+  let currList = []; // list of currentItems at a particular level
+
+  queue.push(currentNode);
+  queue.push(null)
+  while(queue.length >0){
+    currentNode = queue.shift();
+    if(currentNode){
+      currList.add(currentNode.value);
+      if(currentNode.left){
+        queue.push(currentNode.left);
+      }
+      if(currentNode.right){
+        queue.push(currentNode.right);
+      }
+    }
+    else{
+      //we have reached towards the end of the level 
+      list.push(currList);
+      currList = [];
+      if(queue.length) {
+        queue.add(null)
+      }
+    }
+  }
+  return null;
+}
