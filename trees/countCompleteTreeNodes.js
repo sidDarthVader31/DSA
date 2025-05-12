4ae1a8ebe9713ed93c42da295a74d251753faaed
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-05-12 22:06:27 +0530

    add optimized solution for count nodes of a complete binary tree

--- trees/countCompleteTreeNodes.js
+++ trees/countCompleteTreeNodes.js
@@ -26,3 +26,42 @@ var countNumber = function(root) {
     countNumber(root.left, count);
     return countNumber(root.right, count)
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ * runs in log n time 
+ */
+var countNodesOptimized = function(root) {
+
+    if(!root){
+        return 0 ;
+    }
+    let leftHeight =  getHeight(root.left);
+    let rightHeight = getHeight(root.right);
+
+    if(leftHeight == rightHeight){
+      return (1 << leftHeight) + countNodes(root.right);
+    }
+    else{
+          return (1 << rightHeight) + countNodes(root.left);
+    }
+};
+
+const getHeight = (root) => {
+    let height = 0;
+    while(root){
+        height++;
+        root = root.left;
+    }
+    return height;
+}
