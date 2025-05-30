7e2608b27e9ad8ff15069039ba73e210979048ac
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-05-30 11:00:13 +0530

    add alternate solution for dfs in 2d arrays

--- 2darrays/dfs.js
+++ 2darrays/dfs.js
@@ -1,7 +1,8 @@
 
 const dfs = (arr) =>{
   let output = [];
-  recursiveDFS(arr, output, 0, 0, {})
+  // recursiveDFS(arr, output, 0, 0, {})
+  recurisveDFS1(arr, output, 0, 0, {});
   return output
 }
 
@@ -30,6 +31,19 @@ const recursiveDFS = (arr, output, row=0, column=0, map={})=>{
     recursiveDFS(arr, output, row, column-1, map)
   }
   return 
+}
+
+const recurisveDFS1 = (arr, output, row=0, column =0, map={})=>{
+  //base case 
+  if(column <0 || column >=arr[0].length || row <0 || row >=arr.length || map[[row,column]]){
+    return ;
+  }
+  output.push(arr[row][column])
+  map[[row,column]] = 1;
+  recurisveDFS1(arr, output, row-1, column, map);
+  recurisveDFS1(arr, output, row, column+1, map)
+  recurisveDFS1(arr, output, row+1, column, map)
+  recurisveDFS1(arr, output, row, column-1, map);
 }
 
 console.log(`dfs:`, dfs([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]]))
