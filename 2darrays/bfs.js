fecabb02889892e214504f217e470c9e0f2d590b
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-06-03 13:05:13 +0530

    add bfs refined solution

--- 2darrays/bfs.js
+++ 2darrays/bfs.js
@@ -17,7 +17,6 @@ const bfs = (arr) =>{
   queue.push([row1,column1]);
 
   while(queue.length >0){
-    console.log(`ueueue:`, queue)
     let pos = queue.shift();
     let row = pos[0];
     let column = pos[1];
@@ -37,4 +36,33 @@ const bfs = (arr) =>{
   return result;
 }
 
+
+const bfs1 = (arr) =>{
+  const result = [];
+  const seen = new Array(arr.length).fill(0).map(()=>{
+    return new Array(arr[0].length).fill(0)
+  });
+  let queue = [[0,0]];
+
+  while (queue.length) {
+    let pos = queue.shift();
+    let row = pos[0];
+    let column = pos[1];
+
+    if(row <0 || row >= arr.length || column < 0 || column >= arr[0].length || seen[row][column]){
+      continue;
+    }
+
+    seen[row][column] = 1;
+    result.push(arr[row][column]);
+
+    queue.push([row-1, column])
+    queue.push([row, column+1])
+    queue.push([row+1, column])
+    queue.push([row, column-1])
+  }
+  return result;
+}
+
 console.log(bfs([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]]));
+console.log(bfs1([[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20]]));
