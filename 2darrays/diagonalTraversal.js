9a47c89a4f508803811889e348b12659fb614630
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-06-04 10:53:53 +0530

    add alternate solution for diagonal travesal in a matrix

--- 2darrays/diagonalTraversal.js
+++ 2darrays/diagonalTraversal.js
@@ -42,3 +42,39 @@ var findDiagonalOrder = function(matrix) {
 
     return result;
 };
+
+
+
+/**
+ *  * @param {number[][]} mat
+ *   * @return {number[]}
+ *    */
+var findDiagonalOrder = function(matrix) {
+      let map = {}
+      const result = []
+      for(let i =0;i<matrix.length;i++){
+                for(let j = 0;j< matrix[0].length;j++){
+                              let sum = i+j;
+                              if(!map[sum]){
+                                                map[sum] =[];
+                                            }
+                              map[sum].push(matrix[i][j])
+                          }
+            }
+      for(const key of Object.keys(map)){
+                if(key%2 == 0){
+                              result.push(...map[key].reverse())
+                          }
+                else{
+                              result.push(...map[key]);
+                          }
+            }
+      return result;
+};
+                }
+                }
+      }
+                              }
+                }
+      }
+}
