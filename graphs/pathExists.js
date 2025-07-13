1f2b0d9463746762e53c1ffa4c0f50ed01250547
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-07-13 12:42:08 +0530

    addd solution for find if path exists

---
+++ graphs/pathExists.js
@@ -0,0 +1,39 @@
+/**
+ * link : https://leetcode.com/problems/find-if-path-exists-in-graph/
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+var validPath = function(n, edges, source, destination) {
+    const graph = buildGraph(n, edges);
+    return dfs(source, graph, destination, {})
+
+};
+
+function buildGraph(n, edges) {
+    const graph = Array.from({ length: n }, () => []);
+
+    for (const [u, v] of edges) {
+        graph[u].push(v);
+        graph[v].push(u); // because it's undirected
+    }
+
+    return graph;
+}
+const dfs = (vertex, edges, destination, seen) =>{
+    if(vertex == destination) {
+        return true;
+    }
+    let flag = false;
+
+    seen[vertex] = true;
+    let list = edges[vertex];
+    for(let i = 0;i< list.length;i++){
+        if(!seen[list[i]]){
+            flag = flag || dfs(list[i],edges, destination, seen);
+        }
+    }
+    return flag;
+}
