28527354bc5ac06400d916a05eef68249f92fccc
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-07-16 16:29:52 +0530

    modify valid tree solution

--- graphs/validTree.js
+++ graphs/validTree.js
@@ -6,30 +6,32 @@ class Solution {
      * @returns {boolean}
      */
     validTree(n, edges) {
-        const adj = Array.from({ length: n }, () => []);
+      const adj = Array.from({length: n}, ()=>[]);
+      const visited = new Set();
 
-    // Build adjacency list
-    for (const [u, v] of edges) {
+      //create adjacency matrix 
+
+      for(const [u,v] of edges){
         adj[u].push(v);
-        adj[v].push(u); // because it's undirected
-    }
-       const visited = new Array(n).fill(false);
+        adj[v].push(u);
+      }
 
-    const dfs = (node, parent) =>{
-        visited[node]= true;
-        const children = adj[node];
-        for(const neighbor of children){
-          if (!visited[neighbor]) {
-                if (!dfs(neighbor, node)) return false;
-            } else if (neighbor !== parent) {
-                // Cycle detected
-                return false;
-            }
+
+      const dfs = (node, parent) =>{
+        if(visited.has(node)){
+          return false;
+        }
+        visited.add(node);
+        for(const edge of adj[node]){
+          if(edge == parent){
+            //ignore 
+            continue;
+          }
+          if(!dfs(edge, node))return false;
         }
         return true;
-        }
-        if (!dfs(0, -1)) return false;
-        return visited.every(v => v);
+      }
+      return dfs(0,-1) && visited.size == n;
     }
 }
 
