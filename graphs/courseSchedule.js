cdf4e59d8da357647e10b70cfbb823167239f189
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-07-17 12:25:51 +0530

    add course schedule solution with topological sort

--- graphs/courseSchedule.js
+++ graphs/courseSchedule.js
@@ -35,3 +35,43 @@ var canFinish = function(numCourses, prerequisites) {
 };
 
 
+/**
+ * desc: solved using topological sort
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+    const graph = Array.from({ length: numCourses }, () => []);
+    const inDegree = new Array(numCourses).fill(0);
+    for(const [course, pre] of prerequisites){
+        graph[pre].push(course);
+        inDegree[course]++
+    }
+    const queue = [];
+    for(let i=0;i< inDegree.length;i++){
+        if(inDegree[i] ==0){
+            queue.push(i)
+        }
+   }
+    const output = [];
+    const visited = new Set();
+    while(queue.length >0){
+        const vertex = queue.shift();
+        if(visited.has(vertex)){
+            continue;
+        }
+        visited.add(vertex)
+        output.push(vertex);
+        for(const out of graph[vertex]){
+            inDegree[out] = inDegree[out]-1
+            if(inDegree[out]<=0){
+                inDegree[out]=0;
+                queue.push(out)
+            }
+        }
+    }
+    return output.length == numCourses;
+};
+
+
