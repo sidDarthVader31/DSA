438531c83ea4ae1de160c44b72d4896d86ba6eea
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-07-25 12:13:14 +0530

    add solution for no of provinces with union find+ rank

--- graphs/NoOfProvincesWithUnionFind.js
+++ graphs/NoOfProvincesWithUnionFind.js
@@ -38,3 +38,60 @@ class UnionFind {
         }
     }
 }
+
+
+
+
+//No of provinces with union find + rank 
+//
+//
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+var findCircleNum = function(isConnected) {
+    let n = isConnected.length;
+    let uf = new UnionFind(n);
+    for(let i = 0;i< n;i++){
+        for(let j = i+1;j<n;j++){
+            if(isConnected[i][j]){
+                uf.union(i,j);
+            }
+        }
+    }
+    let provinces = new Set();
+    for(let i = 0;i<n;i++){
+        provinces.add(uf.find(i));
+    }
+    return provinces.size;
+}
+
+
+class UnionFind {
+    constructor(n){
+        this.parent = new Array(n).fill(0).map((_,i)=> i);
+        this.rank = new Array(n).fill(1);
+    }
+    find(x){
+        while(x != this.parent[x]){
+            x = this.parent[x];
+        }
+        return x;
+    }
+    union(x,y) {
+        let rootX = this.find(x);
+        let rootY= this.find(y);
+        if(rootY == rootX) return;
+
+        if(this.rank[rootX]< this.rank[rootY]){
+            this.parent[rootX]= rootY;
+        }
+        else if(this.rank[rootX] > this.rank[rootY]){
+            this.parent[rootY] = rootX;
+        }
+        else {
+            this.parent[rootY]= rootX;
+            this.rank[rootX]++;
+        }
+    }
+}
