f4bb62886846e53370d6ece0e3a6ba6999084c0d
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-07-25 12:24:18 +0530

    add no of provinces solution with rank + path compression

--- graphs/NoOfProvincesWithUnionFind.js
+++ graphs/NoOfProvincesWithUnionFind.js
@@ -95,3 +95,61 @@ class UnionFind {
         }
     }
 }
+
+
+
+
+
+// with rank + path compression 
+//
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+var findCircleNum = function(isConnected) {
+    let n = isConnected.length;
+    let uf = new UnionFind(n);
+    for(let i = 0;i< n;i++){
+        for(let j = i+1;j<n;j++){
+            if(isConnected[i][j]){
+                uf.union(i,j);
+            }
+        }
+    }
+    let provinces = new Set();
+    for(let i = 0;i<n;i++){
+        provinces.add(uf.find(i));
+    }
+    return provinces.size;
+}
+
+
+class UnionFind {
+    constructor(n){
+        this.parent = new Array(n).fill(0).map((_,i)=> i);
+        this.rank = new Array(n).fill(1);
+    }
+    find(x){
+        if(this.parent[x]!==x){
+            this.parent[x] = this.find(this.parent[x])
+        }
+        return this.parent[x];
+    }
+
+    union(x,y) {
+        let rootX = this.find(x);
+        let rootY= this.find(y);
+        if(rootY == rootX) return;
+
+        if(this.rank[rootX]< this.rank[rootY]){
+            this.parent[rootX]= rootY;
+        }
+        else if(this.rank[rootX] > this.rank[rootY]){
+            this.parent[rootY] = rootX;
+        }
+        else {
+            this.parent[rootY]= rootX;
+            this.rank[rootX]++;
+        }
+    }
+}
