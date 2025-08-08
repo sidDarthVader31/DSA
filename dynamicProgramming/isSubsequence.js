87242e2d24ba0d7b6a710ebde10668b3d3a1e586
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-08-08 09:09:10 +0530

    add is subsequence solution

---
+++ dynamicProgramming/isSubsequence.js
@@ -0,0 +1,21 @@
+
+/**
+ * link: https://leetcode.com/problems/is-subsequence/
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    const m = s.length, n = t.length;
+    const dp = Array(m + 1).fill(null).map(() => Array(n + 1).fill(0));
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (s[i - 1] === t[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[m][n] === m;
+};
