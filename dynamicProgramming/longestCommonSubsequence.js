5f3355270ebcac503855b3d48feaa9276b2cda42
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-08-08 08:12:05 +0530

    add tabulation method for longest increasing subsequence

--- dynamicProgramming/longestCommonSubsequence.js
+++ dynamicProgramming/longestCommonSubsequence.js
@@ -21,3 +21,28 @@ var longestCommonSubsequence = function(text1, text2) {
     }
     return f(text1.length-1, text2.length-1)
 };
+
+
+/**
+link: https://leetcode.com/problems/longest-common-subsequence/
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequenceTabulation = function(text1, text2) {
+
+    const dp = new Array(text1.length+1).fill(0).map(()=>{
+        return new Array(text2.length+1).fill(0);
+    });
+    for(let i =1;i<= text1.length;i++){
+        for(let j =1;j<=text2.length;j++){
+            if(text1[i-1] == text2[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1]
+            }
+            else{
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
+            }
+        }
+    }
+    return dp[text1.length][text2.length]
+};
