861b3353d8dee51f3ce682ca99161166e8dd6e2d
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-08-08 20:13:35 +0530

    add minimum path sum - tabulation

--- dynamicProgramming/minPathSum.js
+++ dynamicProgramming/minPathSum.js
@@ -22,3 +22,34 @@ var minPathSum = function(grid) {
     }
     return  f(rows-1, cols-1)
 };
+
+
+
+/**tabulation
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+    const rows = grid.length;
+    const cols = grid[0].length;
+    const dp = new Array(rows).fill(0).map(()=>{
+        return new Array(cols).fill(0)
+    });
+
+    let sum=0;
+    for(let i=0;i<rows;i++){
+        sum = sum + grid[i][0]
+        dp[i][0]= sum;
+    }
+    sum =0
+    for(let j=0;j<cols;j++){
+        sum = sum + grid[0][j]
+        dp[0][j]=sum;
+    }
+    for(let i =1;i< rows;i++){
+        for(let j =1;j< cols;j++){
+            dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1])
+        }
+    }
+    return dp[rows-1][cols-1]
+};
