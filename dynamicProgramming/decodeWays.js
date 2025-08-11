fd34a19d494d5b7fd41981523481b69236ee102e
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-08-11 21:58:12 +0530

    add decode ways tabulation

--- dynamicProgramming/decodeWays.js
+++ dynamicProgramming/decodeWays.js
@@ -28,3 +28,36 @@ var numDecodings = function(s) {
     }
     return f(0)
 };
+
+
+
+/**
+    link:https://leetcode.com/problems/decode-ways/
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+    const n = s.length;
+    const dp = new Array(n + 1).fill(0);
+
+    // Base cases
+    dp[n] = 1; // 1 way to decode empty string
+
+    // Fill dp from end to start
+    for (let i = n - 1; i >= 0; i--) {
+        if (s[i] !== '0') {
+            // Take single digit
+            dp[i] += dp[i + 1];
+
+            // Take two digits if valid
+            if (i + 1 < n) {
+                const no = Number(s.substring(i, i + 2));
+                if (no >= 10 && no <= 26) {
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+    }
+
+    return dp[0]; // ways to decode whole string
+};
