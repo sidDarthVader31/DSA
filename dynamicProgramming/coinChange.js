7781f0d09d33f686026b20d30b51f8b017f111b9
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-08-14 12:07:10 +0530

    add coin change solution

---
+++ dynamicProgramming/coinChange.js
@@ -0,0 +1,34 @@
+/**
+ * link: https://leetcode.com/problems/coin-change/
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    const n = coins.length;
+    const dp = new Map();
+    const f = (i, total) => {
+        //f(i, total) -> min no of coins required to achieve total out of 0..i index coins 
+
+        //base cases 
+        if(total == 0) return 0;
+        if(i<0) return Infinity;
+
+        const key = `${i}-${total}`
+        if(dp.has(key)) return dp.get(key);
+        let min = Infinity;
+
+        if(coins[i]<=total) {
+            //take coin 
+            min = Math.min(
+                min, 
+                f(i, total-coins[i]) +1
+            )
+        }
+        min = Math.min(min, f(i-1, total))
+        dp.set(key,min)
+        return min;
+    }
+    const num = f(n-1, amount);
+    return num == Infinity ?-1 : num;
+};
