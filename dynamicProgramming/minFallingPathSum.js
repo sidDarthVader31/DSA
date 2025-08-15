24e6dd690a70fa4fc88c31cab114eb1974abd93c
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-08-15 10:29:50 +0530

    add tabulation ap[roach for min falling path sum

--- dynamicProgramming/minFallingPathSum.js
+++ dynamicProgramming/minFallingPathSum.js
@@ -27,3 +27,40 @@ var minFallingPathSum = function(matrix) {
     }
     return min;
 };
+
+
+/**
+ * tabulation approach
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var minFallingPathSum = function(matrix) {
+    const n = matrix.length;
+    const dp = new Array(n).fill(0).map(()=>{
+        return new Array(n).fill(Infinity)
+    });
+
+    for(let i = 0;i<n;i++){
+        dp[n-1][i] = matrix[n-1][i];
+    }
+
+    for(let i=n-2;i>=0;i--){
+        dp[i][0] = matrix[i][0] + Math.min(
+            dp[i+1][0],
+            dp[i+1][1]
+        );
+        for(let j=1;j<n-1;j++){
+            dp[i][j] = matrix[i][j] + Math.min(
+                dp[i+1][j],
+                dp[i+1][j-1],
+                dp[i+1][j+1]
+            );
+        }
+        dp[i][n-1] = matrix[i][n-1] + Math.min(
+            dp[i+1][n-1],
+            dp[i+1][n-2]
+        )
+    }
+       //console.log(`dp:`, dp);
+    return Math.min(...dp[0])
+};
