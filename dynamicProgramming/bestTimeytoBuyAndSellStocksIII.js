c07b9b263b80547e548fba603414b4e1d75b3453
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-08-17 00:12:05 +0530

    add tabulation approach solution for best time to buy or sell a stock III

--- dynamicProgramming/bestTimeytoBuyAndSellStocksIII.js
+++ dynamicProgramming/bestTimeytoBuyAndSellStocksIII.js
@@ -37,3 +37,33 @@ var maxProfit = function(prices) {
     }
     return f(0,0,0)
 };
+
+
+
+/**
+ * tabulation approach 
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    const n = prices.length;
+    const dp = new Array(n+1).fill(0).map(()=>{ // i
+        return new Array(2).fill(0).map(()=>{ //choices - buy or sell
+            return new Array(3).fill(0)
+        });
+    });
+
+    for(let i = n-1;i>=0;i--){
+        for(let j=0;j<2;j++){
+            dp[i][0][j] = Math.max(
+                -prices[i]+dp[i+1][1][j],//buying
+                dp[i+1][0][j]//ignore
+            )
+            dp[i][1][j] = Math.max(
+                prices[i] + dp[i+1][0][j+1],//selling
+                dp[i+1][1][j]//ignore
+            )
+        }
+    }
+    return dp[0][0][0]
+};
