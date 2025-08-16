3deab0c16a0f3ecf09e71d28ad542c9219d40cc4
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-08-17 00:20:09 +0530

    add memoization + recursion solution for best time to buy or sell stocks IV

--- dynamicProgramming/besttimeToBuyStocksIV.js
+++ dynamicProgramming/besttimeToBuyStocksIV.js
@@ -26,3 +26,44 @@ var maxProfit = function(k, prices) {
     }
     return dp[0][0][0]
 };
+
+
+/**
+ * memoization + recursion
+ * @param {number} k
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(k, prices) {
+      const n = prices.length;
+    const dp = new Array(n+1).fill(0).map(()=>{ // i
+        return new Array(2).fill(0).map(()=>{ //choices - buy or sell
+            return new Array(k).fill(undefined)
+        });
+    });
+
+    const f = (i, j, choices) => {
+        if(i==n) return 0;
+
+        if(choices == k) return 0;
+
+        if(dp[i][j][choices]!= undefined) return dp[i][j][choices]
+        let profit = 0;
+        if(j ==0){
+            //can buy 
+            profit = -prices[i] + f(i+1, 1, choices);
+            return dp[i][j][choices] = Math.max(
+                profit, 
+                f(i+1, j, choices)
+            )
+        }
+        else{
+            profit = prices[i] + f(i+1, 0, choices+1);
+            return dp[i][j][choices] = Math.max(
+                profit,
+                f(i+1, j, choices)
+            )
+        }
+    }
+    return f(0,0,0)
+};
