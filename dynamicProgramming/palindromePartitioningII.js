71e370134b938393e30c6f70d623dde5eb3be0db
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-08-23 20:05:40 +0530

    add palindrome partitioningII solution with tabulation dp g

--- dynamicProgramming/palindromePartitioningII.js
+++ dynamicProgramming/palindromePartitioningII.js
@@ -48,3 +48,42 @@ const isPalindrome = (s, i, j) => {
     }
     return true;
 }
+
+
+
+/**
+ * link : tabulation
+ * @param {string} s
+ * @return {number}
+ */
+var minCut = function (s) {
+    let n = s.length
+    const dp = new Array(n+1).fill(0).map(() => {
+        return new Array(n+1).fill(0)
+    });
+    const pal = Array(n).fill(0).map(() => Array(n).fill(false));
+    for (let len = 1; len <= n; len++) {
+        for (let i = 0; i + len - 1 < n; i++) {
+            let j = i + len - 1;
+            if (s[i] === s[j]) {
+                if (len <= 2) pal[i][j] = true;
+                else pal[i][j] = pal[i + 1][j - 1];
+            }
+        }
+    }
+
+    for(let i = n-1;i>=0;i--){
+        for(let j=i+1;j<n;j++){
+            if(pal[i][j])continue;
+            let min = Infinity;
+            for(let k=i;k<j;k++){
+                if(pal[i][k]){
+                    let minc = 1 + dp[k+1][j];
+                    min = Math.min(min, minc)
+                }
+            }
+            dp[i][j] = min;
+        }
+    }
+    return dp[0][n-1]
+};
