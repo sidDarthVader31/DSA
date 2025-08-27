b843b3ca63a9f544394b273fed898ffc679277e8
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-08-27 19:50:14 +0530

    add subsets with duplicates solution

--- backtracking/subsetII.js
+++ backtracking/subsetII.js
@@ -24,3 +24,27 @@ var subsetsWithDup = function(nums) {
     dfs(0)
     return res;
 };
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+    nums = nums.sort((a,b)=> a-b);
+    const res = [];
+    const path = []
+
+    const dfs = (start) => {
+        res.push([...path]);
+
+        for(let i = start;i<nums.length;i++){
+            if(i> start && nums[i] == nums[i-1])continue;
+            path.push(nums[i]);
+            dfs(i+1);
+            path.pop()
+        }
+    }
+    dfs(0)
+    return res;
+};
