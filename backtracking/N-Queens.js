bf470793f9cbc8545ee57f953f08659ddb00db63
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-08-29 09:16:34 +0530

    add N-queens alternate approach

--- backtracking/N-Queens.js
+++ backtracking/N-Queens.js
@@ -76,3 +76,35 @@ var solveNQueens = function (n) {
 };
 
 
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueens = function (n) {
+    const output =  Array.from({ length: n }, () => '.'.repeat(n));
+    const result = [];
+    const solve = (row, cols = new Set(), diag1= new Set(), diag2 = new Set() ) => {
+        if(row == n){
+            result.push([...output]);
+        return;
+        }
+        for(let col = 0 ;col<n;col++){
+            if(cols.has(col) || diag1.has(row-col)|| diag2.has(row+col))continue;
+            output[row] = output[row].substring(0,col)+ 'Q'+ output[row].substring(col+1);
+            cols.add(col)
+            diag1.add(row-col)
+            diag2.add(row+col)
+            solve(row+1, cols, diag1, diag2);
+
+            //now backtrack
+            output[row]= output[row].substring(0,col)+'.'+ output[row].substring(col+1);
+            cols.delete(col);
+            diag1.delete(row-col);
+            diag2.delete(row+col);
+        }
+    }
+    solve(0);
+    return result;
+};
+
+
