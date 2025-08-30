1b035c8c2013870d4d35ae5a162939d67123de17
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-08-30 12:05:13 +0530

    add lemonade change solution

---
+++ greedy/lemonadeChange.js
@@ -0,0 +1,30 @@
+/**
+ * link : https://leetcode.com/problems/lemonade-change/description/
+ * @param {number[]} bills
+ * @return {boolean}
+ */
+var lemonadeChange = function(bills) {
+    let five=0;
+    let ten = 0;
+    for(const bill of bills) {
+        if(bill == 5)five++;
+        else if (bill==10){
+            if(five == 0) return false;
+            five--;
+            ten++;
+        }
+        else if (bill==20){
+            if(ten >0 && five >0 ){
+                ten--;
+                five--;
+            }
+            else if (five >=3){
+                five = five-3;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    return true;
+};
