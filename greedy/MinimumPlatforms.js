b8b310c40cb7617080dc40b22c6f409a821b7600
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-08-31 21:43:47 +0530

    add min platform solution

---
+++ greedy/MinimumPlatforms.js
@@ -0,0 +1,23 @@
+
+link :https://www.geeksforgeeks.org/problems/minimum-platforms-1587115620/1
+class Solution {
+    findPlatform(arr, dep) {
+        arr.sort((a,b)=> a-b);
+        dep.sort((a,b)=> a-b);
+        let n = arr.length;
+        let i = 0, j = 0;
+        let count = 0, res = 0;
+        
+        while (i < n && j < n) {
+            if (arr[i] <= dep[j]) {
+                count++;
+                res = Math.max(res, count);
+                i++;
+            } else {
+                count--;
+                j++;
+            }
+        }
+        return res;
+    }
+}
