029d6d721066f511cbf50b255d3d727ad2a7d128
Siddharth Bisht <siddharthbisht31@gmail.com>
2025-09-05 10:45:54 +0530

    add disjoint sets with path compression and rank

--- graphs/UnionFind.js
+++ graphs/UnionFind.js
@@ -42,3 +42,34 @@ uf.union(4,5)
 
 console.log(`find 1:`, uf.find(3));
 uf.printParent()
+
+
+
+class  DSU {
+  constructor(n) {
+    this.parent = new Array(n).fill(0).map((_,i)=>i);
+    this.rank = new Array(n).fill(1);
+  }
+  find(u) {
+    if(this.parent[u]!=u){
+      this.parent[u]=this.find(this.parent[u]);
+    }
+    return this.parent[u];
+  }
+  union(u,v) {
+    let rootU = this.find(u);
+    let rootV = this.find(v);
+    if(rootU == rootV) return false;
+    if(this.rank[rootU]< this.rank[rootV]){
+      this.parent[rootU]= rootV;
+    }
+    else if (this.rank[rootU]> this.rank[rootV]){
+      this.rank[rootV] = rootU;
+    }
+    else {
+      this.parent[rootU] = rootV;
+      this.rank[rootV]++;
+    }
+    return true;
+  }
+}
